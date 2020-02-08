206c42910a3498114d5d04acd03da8da575daabe
Freena Lea Berioso <iel.ireb@gmail.com>
2020-02-08 16:41:32 +0800

    test 2

---
+++ src/components/Occurence.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <input
+        type="email"
+        class="form-control"
+        placeholder="tweet something here ..."
+        v-model="Obj"/>
+        <button v-on:click="tweeter(Obj)">Tweet!</button>
+
+        <h1>{{ occuredWord }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            Obj: '',
+            occuredWord: ''
+        }
+    },
+
+    methods: {
+        tweeter (val) {
+            let arr = val.split('').sort().join('');
+
+            return this.occuredWord = arr;
+        }
+
+
+    }
+}
+</script>
